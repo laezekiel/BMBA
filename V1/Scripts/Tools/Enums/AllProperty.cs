48ff18e500edad2a432c5751827f156047faf4f4
laezekiel <louisboure@hotmail.fr>
2023-02-20 00:51:31 +0100

    adding project to git hub

---
+++ V1/Scripts/Tools/Enums/AllProperty.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+namespace Com.IronicEntertainment.BMBA.Tools.Enums
+{
+	public enum AllProperty
+	{
+		Scale,
+		RectScale,
+		GlobalPosition,
+		Modulate,
+		FontColorOverride,
+	}
+
+}
---
+++ V1/Scripts/Tools/Enums/AllSignal.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+namespace Com.IronicEntertainment.BMBA.Tools.Enums
+{
+
+	public enum AllSignal
+	{
+		Timeout,
+		Clicked,
+		TweenFinished,
+		SceneTreetweenFinished,
+	}
+
+}
